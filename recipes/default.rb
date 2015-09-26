e4748b27de823776234f38db804214f99c6e054c
petecarapetyan <pete@couldbe.net>
2015-09-26 05:41:32 -0500

    catch up commit Sept 26 2015

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,3 +7,24 @@ Chef::Log.warn("\n.\n.\n.\n.\n.\n.\n.\nIF YOU DO NOT HAVE A FOLDER NAMED 'shuttl
 include_recipe "df_java::default"
 include_recipe "df_maven::default"
 include_recipe "df_spring_ide::default"
+
+docker_service 'default' do
+  action [:create, :start]
+end
+
+docker_image 'busybox' do
+  action :pull
+end
+
+docker_container 'an echo server' do
+  repo 'busybox'
+  port '1234:1234'
+  command "nc -ll -p 1234 -e /bin/cat"
+end
+
+
+execute "chown docker.sock" do 
+    command "chown -R vagrant:vagrant /run/docker"
+    command "chown -R vagrant:vagrant /run/docker.sock"
+    action :run
+end
